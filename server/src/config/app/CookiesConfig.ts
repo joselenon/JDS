e39a80ba94d201b4ca0a70faa125cdb44abc3a87
joselenon <joselenonn@gmail.com>
2023-09-19 18:17:53 -0300

    Update CookiesConfig.ts

--- server/src/config/app/CookiesConfig.ts
+++ server/src/config/app/CookiesConfig.ts
@@ -1,11 +1,16 @@
+import { CookieOptions } from 'express';
+
 import JWTConfig from './JWTConfig';
 import ICookieConfig from '../interfaces/ICookieConfig';
+import { URLS } from '../constants';
 
 export const JWTCookie = {
   key: 'token',
   config: {
     maxAge: JWTConfig.expiration,
-  },
+    secure: true,
+    domain: URLS.MAIN_URLS.CLIENT_URL,
+  } as CookieOptions,
 };
 
 export const CookiesConfig: ICookieConfig[] = [JWTCookie];

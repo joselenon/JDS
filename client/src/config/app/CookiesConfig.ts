40c80d7ef5e07d48319fa4b9ee077125adb1b1bb
joselenon <joselenonn@gmail.com>
2023-09-19 18:48:04 -0300

    att cookies

--- client/src/config/app/CookiesConfig.ts
+++ client/src/config/app/CookiesConfig.ts
@@ -2,5 +2,7 @@ export const JWTCookie = {
   key: 'token',
   config: {
     maxAge: 5 * 24 * 60 * 60, // 5 days (in seconds)
+    secure: true,
+    domain: 'jdsserverv1.gamblance.com',
   },
 };
--- server/src/config/app/CookiesConfig.ts
+++ server/src/config/app/CookiesConfig.ts
@@ -2,15 +2,13 @@ import { CookieOptions } from 'express';
 
 import JWTConfig from './JWTConfig';
 import ICookieConfig from '../interfaces/ICookieConfig';
-import { URLS } from '../constants';
 
 export const JWTCookie = {
   key: 'token',
   config: {
     maxAge: JWTConfig.expiration,
     secure: true,
-    domain: URLS.MAIN_URLS.SERVER_URL,
-    path: URLS.MAIN_URLS.CLIENT_URL,
+    domain: 'jdsserverv1.gamblance.com',
   } as CookieOptions,
 };
 
